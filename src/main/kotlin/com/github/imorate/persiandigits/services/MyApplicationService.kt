a41744dfff764cf63aa665af0ab401ba9e9f046c
GitHub Action <action@github.com>
2021-06-10 21:12:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/imorate/persiandigits/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/imorate/persiandigits/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.imorate.persiandigits.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imorate.persiandigits.MyBundle
 
 class MyApplicationService {
 
