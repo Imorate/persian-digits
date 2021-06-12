c6da6d1b84b1daf9c5c58c432a02187943486a19
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-06-13 02:21:25 +0430

    Add Popup balloon

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
 <!-- Keep a Changelog guide -> https://keepachangelog.com -->
 
-# persian-digits Changelog
+# Persian-Digits Changelog
 
 ## [Unreleased]
 ## [0.0.1]
 ### Added
 - Initial scaffold created from [IntelliJ Platform Plugin Template](https://github.com/JetBrains/intellij-platform-plugin-template)
+## [1.0.0]
+### Add popup balloon
--- src/main/java/editor/EditorIllustrationAction.java
+++ src/main/java/editor/EditorIllustrationAction.java
@@ -8,6 +8,10 @@ import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.Gray;
+import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 
 public class EditorIllustrationAction extends AnAction {
@@ -19,9 +23,13 @@ public class EditorIllustrationAction extends AnAction {
         Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
         int start = primaryCaret.getSelectionStart();
         int end = primaryCaret.getSelectionEnd();
-        WriteCommandAction.runWriteCommandAction(project, () ->
-                document.replaceString(start, end, PersianDigits.convert(primaryCaret.getSelectedText()))
-        );
+        JBPopupFactory jbPopupFactory = JBPopupFactory.getInstance();
+        Balloon balloon = jbPopupFactory.createHtmlTextBalloonBuilder("Converted", null,
+                new JBColor(Gray._242, Gray._0), null).setFadeoutTime(1000).createBalloon();
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            document.replaceString(start, end, PersianDigits.convert(primaryCaret.getSelectedText()));
+            balloon.show(jbPopupFactory.guessBestPopupLocation(editor), Balloon.Position.below);
+        });
         primaryCaret.removeSelection();
     }
 
